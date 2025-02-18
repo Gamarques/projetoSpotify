fa471bdb7bd874b7b3db0e70d6040589092e1453
gamarques1 <gabrielgamarques1@gmail.com>
2025-02-18 04:08:00 -0300

    criando ofrontend

---
+++ frontend/src/assets/database/songs.js
@@ -0,0 +1,13 @@
+export const songsArray = [
+    {
+      image: "https://i.scdn.co/image/ab67616d00001e022774b00531d558bc19e12a24",
+      name: "Última Saudade - Ao Vivo",
+      duration: "02:30",
+      artist: "Henrique & Juliano",
+      audio:
+        "https://jornada-fullstack.s3.us-east-2.amazonaws.com/ultima-saudade.mp3",
+      id: 1,
+    },
+];
+
+
