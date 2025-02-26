38155877ebaa410e3d052364b4ac4307744cd1de
gamarques1 <gabrielgamarques1@gmail.com>
2025-02-26 00:57:45 -0300

    API front funcionando do jeito simples

--- frontend/src/assets/database/songs.js
+++ frontend/src/assets/database/songs.js
@@ -1,5 +1,17 @@
-export const songsArray = [
+import { songsArray } from "../../services/api";
 
-];
+export {songsArray} ;
 
+// import { songsApi } from "../../services/songsApi";
 
+// export const songsArray = [
+//     async () => {
+//         try {
+//             const response = await songsApi.getAll();
+//             return response;
+//         } catch (error) {
+//             console.error('Erro ao buscar músicas:', error);
+//             throw error;
+//         }
+//     }
+// ];
